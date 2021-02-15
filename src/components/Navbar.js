4454a8f60bd304c97b58146a41176a53a4dd7e63
projectaki <akosegypro@gmail.com>
2021-02-15 17:55:39 +0100

    added back button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,18 @@ class Navbar extends Component {
         return (
             <div>
                 <nav>
-                    <div class="nav-wrapper" style={{textAlign: "center"}}>
-                        <span style={{fontSize: "3vh"}}>Pathfinder Visualiser</span>
+                    <div class="nav-wrapper">
+                        <div style={{display: "grid", gridTemplateColumns: "20vw 80vw"}}>
+                            <div style={{fontSize: "1vw"}}>
+                                <a href="https://projectaki.github.io/portfolio_akos_madarasz/#/Projects">
+                                    <i class="fas fa-arrow-left fa-3x" style={{paddingLeft: "1vw", width: "100%", height: "100%"}}></i>
+                                </a>
+                            </div>
+                            <div>
+                                <span style={{fontSize: "3vh", marginLeft: "25vw"}}>Pathfinder Visualiser</span>
+                            </div>
+                        </div>
+                        
                     
                     </div>
                 </nav>
